9e2807f929422e81f00a85cdcd5eb21938dd9002
JenyTopol <51214133+jenyaTopol@users.noreply.github.com>
2021-08-25 20:30:40 +0300

    Add files via upload

---
+++ newfile2.tf
@@ -0,0 +1,48 @@
+#my terraform
+#build web server
+#buil by jenyatopol
+provider "aws" {
+    
+    region = "us-east-1"
+  
+}
+resource "aws_instance" "my_web_server_ubu" {
+    count                  = 0
+    ami                    = "ami-09e67e426f25ce0d7"             #ubuntu server          
+    instance_type          = "t3.micro"
+    key_name               =  "terraform_key_pait"
+    vpc_security_group_ids = [aws_security_group.my_security_group.id]
+    user_data = <<EOF
+#!/bin/bash
+sudo apt install docker.io
+EOF
+}
+resource "aws_security_group" "my_security_group" {
+  name        = "my_security_group"
+  description = "Allow my_first_security_group"
+  
+
+
+
+ingress {
+    
+      description      = "TLS from VPC"
+      from_port        = 0
+      to_port          = 6000
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  
+
+  egress{ 
+    
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  
+}
+   
