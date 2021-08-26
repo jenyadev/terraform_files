fd894d979088da00302233fa24f23357f684ee4f
JenyTopol <51214133+jenyaTopol@users.noreply.github.com>
2021-08-26 14:58:27 +0300

    Update newfile2.tf

--- newfile2.tf
+++ newfile2.tf
@@ -21,19 +21,23 @@ resource "aws_security_group" "my_security_group" {
   name        = "my_security_group"
   description = "Allow my_first_security_group"
   
-
-
-
 ingress {
     
       description      = "TLS from VPC"
-      from_port        = 0
-      to_port          = 6000
+      from_port        = 22
+      to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
     }
-  
+   ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+ }
 
   egress{ 
     
